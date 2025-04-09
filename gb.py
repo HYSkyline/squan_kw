fe77f19de957ab21343a32917bd0900580c752e8
Han Junyu <HYSkyline@hotmail.com>
2025-04-09 18:20:17 +0800

    demo_20250409
    增加了对于pdf和word的支持，但面临pdf表格难以处理的问题。确认放弃对html的解析（不同城市的网页建设无统一标准）
    接下来尝试评估报告的指标计算

--- gb.py
+++ gb.py
@@ -4,7 +4,7 @@ import time
 import os
 import requests
 from openai import OpenAI
-# from docx import Document
+from docx import Document
 from markitdown import MarkItDown
 import re
 import json
@@ -17,30 +17,28 @@ def main(f_path):
     time_origin = time.time()
 
     txt_content = format_trans(f_path)
-    # print(u'******调试用********\n' + txt_content[-1])
-    for i in range(1, len(txt_content)):
-        time_para = time.time()
-        para_name = txt_content[i][:txt_content[i].find('\n')]
-        print(u'文字信息正在提交大模型. 当前章节:' + para_name)
-        # data_res = data_extract_ollama(txt_content[i])
-        # output_check = data_output_ollama(para_name, data_res)
-        data_res = data_extract_aliyun(txt_content[i])
-        output_check = data_output_aliyun(para_name, data_res)
-        print(u'本篇章数据已提取。共耗时:' + str(int((time.time() - time_para) * 100) / 100) + 's.')
-        print('--' * 6)
-        time.sleep(1)
+    print(u'******调试用********\n' + txt_content[4])
+    # for i in range(1, len(txt_content)):
+    #     time_para = time.time()
+    #     para_name = txt_content[i][:txt_content[i].find('\n')]
+    #     print(u'文字信息正在提交大模型. 当前章节:' + para_name)
+    #     # data_res = data_extract_ollama(txt_content[i])
+    #     # output_check = data_output_ollama(para_name, data_res)
+    #     data_res = data_extract_aliyun(txt_content[i])
+    #     output_check = data_output_aliyun(para_name, data_res)
+    #     print(u'本篇章数据已提取。共耗时:' + str(int((time.time() - time_para) * 100) / 100) + 's.')
+    #     print('--' * 6)
     print(u'\n总计耗时:' + str(int((time.time() - time_origin) * 100) / 100) + 's.\n程序已完成.')
 
 
 def format_trans(file_path):
     print(u'对输入文件进行预处理...')
-    file_all_name = os.path.split(file_path)[1]
-    file_name, file_format = file_all_name.split('.')
+    file_name, file_format = file_path.split('.')
     content = ''
 
     if file_format == 'html':
         print(u'网址: ' + file_name)
-        print(u'待处理文件为网页文件，即将联网打开该网页')
+        print(u'待处理文件为网页文件，正在尝试联网打开...')
         content = file_read_html(file_path)
     elif file_format == 'docx' or file_format == 'doc':
         print(u'文件名:' + file_name)
@@ -56,10 +54,12 @@ def format_trans(file_path):
         content = file_read_pdf(file_path)
     else:
         print(u'还未想好怎么处理的文件格式')
-
     print('--' * 6)
-    content_paras = content_split(content)
-    return content_paras
+    if content:
+        content_paras = content_split(content)
+        return content_paras
+    else:
+        exit()
 
 
 def data_extract_ollama(txt_content):
@@ -68,7 +68,7 @@ def data_extract_ollama(txt_content):
     with open('prompt_pre.txt', 'r', encoding='utf-8') as f_prompt:
         prompt_pre = f_prompt.read()
     # print(u'*******调试用*******\n' + prompt_pre + txt_content.replace(' ', '') + '\n******************')
-    url = "http://" + ollama_ips[0][:-1] + ":11434/api/generate"  # 实际使用时，IP 替换为 Ollama 所在的服务器 IP
+    url = "http://" + ollama_ips[1][:-1] + ":11434/api/generate"  # 实际使用时，IP 替换为 Ollama 所在的服务器 IP
     payload = {
         "model": "qwq:latest",
         "prompt": prompt_pre + txt_content,
@@ -144,24 +144,25 @@ def file_read_txt(f_path):
 
 
 def file_read_html(f_path):
-    try:
-        response = requests.post(f_path)
-        return response.text
-    except Exception as e:
-        print(u'网页读取失败:' + str(e))
-        exit()
+    print(u'目前尚未完成对网页内容的解析，建议自行用浏览器打开网页，并手动将公报内容复制到word中保存，并使用该word文件作为输入进行读取.')
+    print(u'程序本次运行无法进入后续环节，即将退出.')
+    return ''
 
 
 def file_read_docx(f_path):
-    md = MarkItDown()
-    result = md.convert(f_path)
-    return result.text_content
+    document = Document(f_path)
+    full_text = []
+    for paragraph in document.paragraphs:
+        full_text.append(paragraph.text)
+    return '\n'.join(full_text)
 
 
 def file_read_pdf(f_path):
     md = MarkItDown()
-    result = md.convert(f_path)
-    return result.text_content
+    res_clean = md.convert(f_path).text_content
+    res_clean = re.sub(r'\s*—\s*\d+\s*—\s*', '', res_clean)
+    res_clean = re.sub(r'\s*-\s*\d+\s*-\s*', '', res_clean)
+    return res_clean
 
 
 def content_split(txt):
@@ -174,7 +175,8 @@ def content_split(txt):
 
 
 if __name__ == '__main__':
-    # f_path = 'https://www.ly.gov.cn/2024/05-09/144721.html'
-    f_path = u'D:\\squan_kw\\2023年洛阳市国民经济和社会发展统计公报.txt'
+    # f_input = 'https://www.ly.gov.cn/2024/05-09/144721.html'
+    # f_input = u'2023年洛阳市国民经济和社会发展统计公报.docx'
+    f_input = u'黔东南苗族侗族自治州2023年国民经济和社会发展统计公报.pdf'
     global time_origin
-    main(f_path)
+    main(f_input)
