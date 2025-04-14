e85356cedc8982623f922983aa3acf2bffa02acc
Han Junyu <HYSkyline@hotmail.com>
2025-04-14 17:54:23 +0800

    md test 20250414
    测试以md文档取代txt作为数据抽取的统一源格式
    需要再优化pdf的格式转换，用pdf2docx存在表格单位错误的问题

--- md_test.py
+++ md_test.py
@@ -3,6 +3,8 @@
 from markitdown import MarkItDown
 import time
 import os
+import requests
+from pdf2docx import Converter
 
 
 def main(file_list):
@@ -10,22 +12,42 @@ def main(file_list):
 	print('--' * 6)
 	time_ori = time.time()
 
+	# word文件转md
 	md_word = file_trans(file_list['word'])
-	md_pdf = file_trans(file_list['pdf'])
-	md_html = file_trans(file_list['html'])
+	with open('temp/md_res_word.txt', 'w', encoding='utf-8') as f_res:
+		f_res.write(md_word)
+	print('word file converted.')
 
-	print('Word transform result:\n' + md_word + '\n' + '--' * 6)
-	print('PDF transform result:\n' + md_pdf + '\n' + '--' * 6)
-	print('html transform result:\n' + md_html + '\n' + '--' * 6)
+	# PDF文件先转word再转md
+	cv = Converter(file_list['pdf'])
+	cv.convert('temp/md_tmp_pdf.docx')
+	cv.close()
+	md_pdf = file_trans('temp/md_tmp_pdf.docx')
+	with open('temp/md_res_pdf.txt', 'w', encoding='utf-8') as f_res:
+		f_res.write(md_pdf)
+	print('pdf file converted.')
 
-	print('Time escape:' + str(int((time.time() - time_ori) * 100) / 100))
+	# HTML文件打开网页转md
+	md_html = file_trans(requests.get(file_list['html']))
+	with open('temp/md_res_html.txt', 'w', encoding='utf-8') as f_res:
+		f_res.write(md_html)
+	print('html file converted.')
+	print('--' * 6)
+
+	print('Time escape:' + str(int((time.time() - time_ori) * 100) / 100) + 's.')
 	print('Link Logout.')
+
+
+def file_trans(f_o):
+	md = MarkItDown()
+	res = md.convert(f_o).text_content
+	return res
 
 
 if __name__ == '__main__':
 	file_list = {
-		'word': '',
-		'pdf': '',
-		'html': ''
+		'word': u'material/md素材-2023年洛阳市国民经济和社会发展统计公报.docx',
+		'pdf': u'material/黔东南苗族侗族自治州2023年国民经济和社会发展统计公报.pdf',
+		'html': 'https://tjj.qdn.gov.cn/tjsj/tjgb_57099/tjgb_57101/202105/t20210508_68020510.html'
 	}
 	main(file_list)
